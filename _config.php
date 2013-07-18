96708fa652be8c0ebf2c84c7537433e71a58b6c8
pett <pett@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-07-18 11:27:35 +0000

    UPDATE: 3.1 configs
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/googleanalyticsbasics/trunk@4905 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- _config.php
+++ _config.php
@@ -8,11 +8,7 @@
 
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START googleanalyticsbasics MODULE ----------------===================
-//MUST SET
-//Object::add_extension('SiteTree', 'GoogleAnalyticsSTE');
-//GoogleAnalyticsSTE::set_main_code(''); //e.g. UA-xxxx-y
 //MAY SET
-//GoogleAnalyticsSTE::set_optional_code(''); //e.g. UA-xxxx-y
 // - to track downloads - you may want to add this to your Page_Controller::init
 //Requirements::javascript('googleanalyticsbasics/javascript/trackDownloads.js');
 //===================---------------- END googleanalyticsbasics MODULE ----------------===================
