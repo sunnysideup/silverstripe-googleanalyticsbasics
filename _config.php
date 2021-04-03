e5afa828dd5fbd0f6fe4c68a9dfe1a7e405dde70
nicolaas @ tappy <github@sunnysideup.co.nz>
2021-04-04 10:35:25 +1200

    LINTER MORE

--- _config.php
+++ _config.php
@@ -2,10 +2,10 @@
 
 namespace Sunnysideup\GoogleAnalyticsBasics;
 
-/**
+/*
  * developed by www.sunnysideup.co.nz
  * author: Nicolaas - modules [at] sunnysideup.co.nz
-**/
+ */
 
 //MAY SET
 // - to track downloads - you may want to add this to your Page_Controller::init
